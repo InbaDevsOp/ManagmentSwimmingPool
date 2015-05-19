f02197fb32fc44b9d77572e6dd6312675d34f744
falfaro <falfaro@lan.com>
2015-05-19 05:49:11 -0300

    add validate login mula

--- app/dev/js/Views/login.js
+++ app/dev/js/Views/login.js
@@ -1,7 +1,7 @@
 define(['backbone', 'jquery', 'hbs!Templates/headerApplication', 'hbs!Templates/initialApplicationPage',
-        'hbs!Templates/login'
+        'hbs!Templates/login','Views/loginValidation'
     ],
-    function(Backbone, $, headerApplication, initialApplicationPage, loginTemplate) {
+    function(Backbone, $, headerApplication, initialApplicationPage, loginTemplate, loginValidation) {
 
         LoginView = Backbone.View.extend({
             el: $("#applicationContent"),
@@ -11,9 +11,9 @@ define(['backbone', 'jquery', 'hbs!Templates/headerApplication', 'hbs!Templates/
             },
 
             initialize: function(argument) {
+                loginValidation.validateForm();
                 var flagSession = localStorage.getItem('sessionActive');
                 var userName = localStorage.getItem('names');
-                
                 if (flagSession == 1) {
                     $('#header').html(headerApplication({userName: userName}));
                     $(this.el).html(initialApplicationPage());
@@ -24,33 +24,35 @@ define(['backbone', 'jquery', 'hbs!Templates/headerApplication', 'hbs!Templates/
             },
             login: function() {
                 var that = this;
-                var rut = $("#rut").val();
-                var password = $("#password").val();
+                if(loginValidation.isValidForm()){
+                    var rut = $("#rut").val();
+                    var password = $("#password").val();
 
-                var getParamsService = rut + "/" + password;
-                var url = SwimmingPoolApplicationHost + "/SwimmingPoolServiceExample/rest/users/login/" + getParamsService;
+                    var getParamsService = rut + "/" + password;
+                    var url = SwimmingPoolApplicationHost + "/SwimmingPoolServiceExample/rest/users/login/" + getParamsService;
 
-                $.ajax({
-                    async: false,
-                    url: url,
-                    type: "GET",
-                    success: function(data, status) {
-                        if (data.result == true) {
-                            var userName = data.idUser;
+                    $.ajax({
+                        async: false,
+                        url: url,
+                        type: "GET",
+                        success: function(data, status) {
+                            if (data.result == true) {
+                                var userName = data.idUser;
 
-                            $('#header').html(headerApplication({userName: userName}));
-                            $(that.el).html(initialApplicationPage());
-                            
-                            localStorage.setItem('sessionActive', 1);
-                            localStorage.setItem('userName', userName);
-                        } else {
-                            alertDGC("Usuario no existente, favor consultar administración para recuperación de clave");
-                        }
-                    },
-                    error: function(request, error) {
-                        alertDGC("Error Interno, favor intente más tarde");
-                    },
-                });
+                                $('#header').html(headerApplication({userName: userName}));
+                                $(that.el).html(initialApplicationPage());
+                                
+                                localStorage.setItem('sessionActive', 1);
+                                localStorage.setItem('userName', userName);
+                            } else {
+                                alertDGC("Usuario no existente, favor consultar administración para recuperación de clave");
+                            }
+                        },
+                        error: function(request, error) {
+                            alertDGC("Error Interno, favor intente más tarde");
+                        },
+                    });
+                }
             },
         });
 
