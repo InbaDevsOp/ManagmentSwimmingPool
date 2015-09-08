631865f821865ef6c10bc4e3739c3f1e579f913c
falfaro <falfaro@lan.com>
2015-09-07 22:55:15 -0300

    delete sunday in schedule config

--- app/dev/js/Modules/schedule/scheduleConfig.js
+++ app/dev/js/Modules/schedule/scheduleConfig.js
@@ -3,46 +3,46 @@ define([],
 
         return scheduleTableConfig = [{
             "title": "7:00-8:00",
-            "daySectionId": [1, 2, 3, 4, 5, 6, 7]
+            "daySectionId": [1, 2, 3, 4, 5, 6]
         }, {
             "title": "8:00-9:00",
-            "daySectionId": [8, 9, 10, 11, 12, 13, 14]
+            "daySectionId": [8, 9, 10, 11, 12, 13]
         }, {
             "title": "9:00-10:00",
-            "daySectionId": [15, 16, 17, 18, 19, 20, 21]
+            "daySectionId": [15, 16, 17, 18, 19, 20]
         }, {
             "title": "10:00-11:00",
-            "daySectionId": [22, 23, 24, 25, 26, 27, 28]
+            "daySectionId": [22, 23, 24, 25, 26, 27]
         }, {
             "title": "11:00-12:00",
-            "daySectionId": [29, 30, 31, 32, 33, 34, 35]
+            "daySectionId": [29, 30, 31, 32, 33, 34]
         }, {
             "title": "12:00-13:00",
-            "daySectionId": [36, 37, 38, 39, 40, 41, 42]
+            "daySectionId": [36, 37, 38, 39, 40, 41]
         }, {
             "title": "13:00-14:00",
-            "daySectionId": [43, 44, 45, 46, 47, 48, 49]
+            "daySectionId": [43, 44, 45, 46, 47, 48]
         }, {
             "title": "14:00-15:00",
-            "daySectionId": [50, 51, 52, 53, 54, 55, 56]
+            "daySectionId": [50, 51, 52, 53, 54, 55]
         }, {
             "title": "15:00-16:00",
-            "daySectionId": [57, 58, 59, 60, 61, 62, 63]
+            "daySectionId": [57, 58, 59, 60, 61, 62]
         }, {
             "title": "16:00-17:00",
-            "daySectionId": [64, 65, 66, 67, 68, 69, 70]
+            "daySectionId": [64, 65, 66, 67, 68, 69]
         }, {
             "title": "17:00-18:00",
-            "daySectionId": [71, 72, 73, 74, 75, 76, 77]
+            "daySectionId": [71, 72, 73, 74, 75, 76]
         }, {
             "title": "18:00-19:00",
-            "daySectionId": [78, 79, 80, 81, 82, 83, 84]
+            "daySectionId": [78, 79, 80, 81, 82, 83]
         }, {
             "title": "19:00-20:00",
-            "daySectionId": [85, 86, 87, 88, 89, 90, 91]
+            "daySectionId": [85, 86, 87, 88, 89, 90]
         }, {
             "title": "20:00-21:00",
-            "daySectionId": [92, 93, 94, 95, 96, 97, 98]
+            "daySectionId": [92, 93, 94, 95, 96, 97]
         },];
 
     });
