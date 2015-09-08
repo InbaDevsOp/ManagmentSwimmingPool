48561fc35c596fe23d458739f9a0a3a94b8b54ce
falfaro <falfaro@lan.com>
2015-09-07 23:50:15 -0300

    change leter u for U dir app/dev/js/Views/swimmingPoolUser

--- app/dev/js/Views/swimmingPoolUser/addUser.js
+++ app/dev/js/Views/swimmingPoolUser/addUser.js
@@ -1,5 +1,5 @@
 define(['backbone', 'jquery', 'Modules/login', 'Modules/utilForm',
-        'hbs!Templates/swimmingPooluser/createdUser', 'Views/swimmingPoolUser/addSwimmingPoolUserValidation',
+        'hbs!Templates/swimmingPoolUser/createdUser', 'Views/swimmingPoolUser/addSwimmingPoolUserValidation',
         'hbs!Templates/comboRegion', 'Views/regionInformation','hbs!Templates/comboCommunes'
     ],
     function(Backbone, $, login, utilForm, createdUser, addSwimmingPoolUserValidation,templateComboRegion, viewRegionInformation,
